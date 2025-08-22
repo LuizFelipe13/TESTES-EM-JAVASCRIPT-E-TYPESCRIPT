344b0a32cb1997c76bbf19961cac55ef81834b8b
LuizFelipe13 <luiz_felipesbc@hotmail.com>
2025-08-22 17:26:30 -0300

    Objeto window (objeto global)

---
+++ OBJETO_WINDOW/script.js
@@ -0,0 +1,17 @@
+console.log(window);
+var teste = "Olá Mundo";
+console.log(window);
+
+
+console.log(this.teste);
+
+let teste2 = "Olá Mundo 2";
+const teste3 = "Olá Mundo 3";
+
+console.log(window);
+
+function testando() {
+    console.log("teste");
+}
+
+console.log(window);
